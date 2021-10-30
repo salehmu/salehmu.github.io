5c3a8a720991c1047a70218cde68b3d0d12d0d8a
salehmu <74098495+salehmu@users.noreply.github.com>
2021-10-30 07:10:40 +0200

    "update"

--- js/search.js
+++ js/search.js
@@ -1,140 +1,154 @@
-// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
-// How many characters to include on either side of match keyword
-const summaryInclude=60;
+// A local search script with the help of
+// [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
+// Copyright (C) 2015
+// Joseph Pan <http://github.com/wzpan>
+// Shuhao Mao <http://github.com/maoshuhao>
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+// Modified by:
+// Pieter Robberechts <http://github.com/probberechts>
 
-// Options for fuse.js
-let fuseOptions = {
-  shouldSort: true,
-  includeMatches: true,
-  tokenize: true,
-  matchAllTokens: true,
-  threshold: 0.0,
-  location: 0,
-  distance: 100,
-  maxPatternLength: 64,
-  minMatchCharLength: 3,
-  keys: [
-    {name:"title",weight:0.8},
-    {name:"tags",weight:0.5},
-    {name:"categories",weight:0.5},
-    {name:"contents",weight:0.4}
-  ]
+/*exported searchFunc*/
+var searchFunc = function(path, searchId, contentId) {
+
+  function stripHtml(html) {
+    html = html.replace(/<style([\s\S]*?)<\/style>/gi, "");
+    html = html.replace(/<script([\s\S]*?)<\/script>/gi, "");
+    html = html.replace(/<figure([\s\S]*?)<\/figure>/gi, "");
+    html = html.replace(/<\/div>/ig, "\n");
+    html = html.replace(/<\/li>/ig, "\n");
+    html = html.replace(/<li>/ig, "  *  ");
+    html = html.replace(/<\/ul>/ig, "\n");
+    html = html.replace(/<\/p>/ig, "\n");
+    html = html.replace(/<br\s*[\/]?>/gi, "\n");
+    html = html.replace(/<[^>]+>/ig, "");
+    return html;
+  }
+
+  function getAllCombinations(keywords) {
+    var i, j, result = [];
+
+    for (i = 0; i < keywords.length; i++) {
+        for (j = i + 1; j < keywords.length + 1; j++) {
+            result.push(keywords.slice(i, j).join(" "));
+        }
+    }
+    return result;
+  }
+
+  $.ajax({
+    url: path,
+    dataType: "xml",
+    success: function(xmlResponse) {
+      // get the contents from search data
+      var datas = $("entry", xmlResponse).map(function() {
+        return {
+          title: $("title", this).text(),
+          content: $("content", this).text(),
+          url: $("link", this).attr("href")
+        };
+      }).get();
+
+      var $input = document.getElementById(searchId);
+      if (!$input) { return; }
+      var $resultContent = document.getElementById(contentId);
+
+      $input.addEventListener("input", function(){
+        var resultList = [];
+        var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
+          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
+        $resultContent.innerHTML = "";
+        if (this.value.trim().length <= 0) {
+          return;
+        }
+        // perform local searching
+        datas.forEach(function(data) {
+          var matches = 0;
+          if (!data.title || data.title.trim() === "") {
+            data.title = "Untitled";
+          }
+          var dataTitle = data.title.trim().toLowerCase();
+          var dataContent = stripHtml(data.content.trim());
+          var dataUrl = data.url;
+          var indexTitle = -1;
+          var indexContent = -1;
+          var firstOccur = -1;
+          // only match artiles with not empty contents
+          if (dataContent !== "") {
+            keywords.forEach(function(keyword) {
+              indexTitle = dataTitle.indexOf(keyword);
+              indexContent = dataContent.indexOf(keyword);
+
+              if( indexTitle >= 0 || indexContent >= 0 ){
+                matches += 1;
+                if (indexContent < 0) {
+                  indexContent = 0;
+                }
+                if (firstOccur < 0) {
+                  firstOccur = indexContent;
+                }
+              }
+            });
+          }
+          // show search results
+          if (matches > 0) {
+            var searchResult = {};
+            searchResult.rank = matches;
+            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
+            if (firstOccur >= 0) {
+              // cut out 100 characters
+              var start = firstOccur - 20;
+              var end = firstOccur + 80;
+
+              if(start < 0){
+                start = 0;
+              }
+
+              if(start == 0){
+                end = 100;
+              }
+
+              if(end > dataContent.length){
+                end = dataContent.length;
+              }
+
+              var matchContent = dataContent.substr(start, end);
+
+              // highlight all keywords
+              var regS = new RegExp(keywords.join("|"), "gi");
+              matchContent = matchContent.replace(regS, function(keyword) {
+                return "<em class=\"search-keyword\">"+keyword+"</em>";
+              });
+
+              searchResult.str += "<p class=\"search-result\">" + matchContent +"...</p>";
+            }
+            searchResult.str += "</li>";
+            resultList.push(searchResult);
+          }
+        });
+        resultList.sort(function(a, b) {
+            return b.rank - a.rank;
+        });
+        var result ="<ul class=\"search-result-list\">";
+        for (var i = 0; i < resultList.length; i++) {
+          result += resultList[i].str;
+        }
+        result += "</ul>";
+        $resultContent.innerHTML = result;
+      });
+    }
+  });
 };
-
-function getUrlParameter(name) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-  let results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-let searchQuery = getUrlParameter('q');
-
-if(searchQuery){
-  document.getElementById("search-query").value = searchQuery;
-  executeSearch(searchQuery);
-} else {
-  document.getElementById('search-results').innerHTML = "<p class=\"no-results\">Please enter a word or phrase above</p>";
-}
-
-function executeSearch(searchQuery) {
-  // Look for "index.json" in the same directory where this script is called.
-  fetch("index.json").
-  then(function (response) {
-    return response.json()
-  }).
-  then(function (data) {
-    let fuse = new Fuse(data, fuseOptions);
-    let result = fuse.search(searchQuery);
-    if (result.length > 0) {
-      populateResults(result);
-    } else {
-      document.getElementById('search-results').innerHTML = "<p class=\"no-results\">No matches found</p>";
-    }
-  });
-}
-
-function populateResults(result){
-  result.forEach( function (value, key) {
-    let contents= value.item.contents;
-    let snippet = "";
-    let snippetHighlights=[];
-    snippetHighlights.push(searchQuery);
-    if(snippet.length<1){
-      var getSentenceByWordRegex = new RegExp( 
-        `[^.?!]*(?<=[.?\\s!])${searchQuery}(?=[\\s.?!])[^.?!]*[.?!]`,
-        'i'
-      );
-      var maxTextLength = summaryInclude*2
-      // Index of the matched search term
-      var indexOfMatch = contents.toLowerCase().indexOf(
-        searchQuery.toLowerCase()
-      );
-      // Index of the first word of the sentence with the search term in it
-      var indexOfSentence = contents.indexOf(
-        getSentenceByWordRegex.exec(contents)
-      );
-      
-      var start 
-      var cutStart = false
-      // Is the match in the result?
-      if(indexOfSentence+maxTextLength < indexOfMatch){
-        // Make sure that the match is in the result
-        start = indexOfMatch
-        // This bool is used to replace the first part with '...'
-        cutStart = true 
-      } else {
-        // Match is in view, even if we show the whole sentence
-        start = indexOfSentence
-      }
-      
-      // Change end length to the text length if it is longer than 
-      // the text length to prevent problems
-      var end = start + maxTextLength 
-      if (end > contents.length){
-        end = contents.length
-      }
-
-      if(cutStart){
-        // Replace first three characters with '...'
-        end -= 3;
-        snippet += "…" + contents.substring(start, end).trim();
-      }
-      else{
-        snippet += contents.substring(start, end).trim();
-      }     
-    }
-    snippet += "…";
-
-    // Lifted from https://stackoverflow.com/posts/3700369/revisions
-    var elem = document.createElement('textarea');
-    elem.innerHTML = snippet;
-    var decoded = elem.value;
-
-    // Pull template from hugo template definition
-    let frag = document.getElementById('search-result-template').content.cloneNode(true);
-    // Replace values
-    frag.querySelector(".search_summary").setAttribute("id", "summary-" + key);
-    frag.querySelector(".search_link").setAttribute("href", value.item.permalink);
-    frag.querySelector(".search_title").textContent = value.item.title;
-    frag.querySelector(".search_snippet").textContent = decoded;
-    let tags = value.item.tags;
-    if (tags) {
-      frag.querySelector(".search_tags").textContent = tags;
-    } else {
-      frag.querySelector(".search_iftags").remove();
-    }
-    let categories = value.item.categories;
-    if (categories) {
-      frag.querySelector(".search_categories").textContent = categories;
-    } else {
-      frag.querySelector(".search_ifcategories").remove();
-    }
-    snippetHighlights.forEach( function (snipvalue, snipkey) {
-      let markjs = new Mark(frag);
-      markjs.mark(snipvalue);
-    });
-    document.getElementById("search-results").appendChild(frag);
-  });
-}
-// @license-end
